05fa6d23ce0128a5eeb24dd6db8d5d5d36fa52d9
trixky <git config --global user.name>
2024-08-14 22:09:46 +0200

    [Dev] lesson 44: finish

--- src/lib/shaders/procedural/vertex.glsl
+++ src/lib/shaders/procedural/vertex.glsl
@@ -1,3 +1,53 @@
+#include ./simplexNoise2d.glsl
+
+#define timeSpeed 0.2;
+
+uniform float uTime;
+uniform float uPositionFrequency;
+uniform float uStrength;
+uniform float uWarpFrequency;
+uniform float uWarpStrength;
+
+varying vec3 vPosition;
+varying float vUpDot;
+
+float getElevation(vec2 position) {
+
+    vec2 warpPosition = position;
+    warpPosition += uTime * timeSpeed;
+
+    warpPosition += simplexNoise2d(warpPosition * uPositionFrequency * uWarpFrequency) * uWarpStrength;
+
+    float elevation = 0.0;
+    elevation += simplexNoise2d(warpPosition * uPositionFrequency) / 2.;
+    elevation += simplexNoise2d(warpPosition * uPositionFrequency * 2.0) / 4.;
+    elevation += simplexNoise2d(warpPosition * uPositionFrequency * 4.0) / 8.;
+
+    float elevationSign = sign(elevation);
+    elevation = pow(elevation, 2.0) * elevationSign;
+    elevation *= uStrength;
+
+    return elevation;
+}
+
 void main() {
-    
+    // Neighbours positions
+    float shift = 0.01;
+    vec3 PositionA = position + vec3(shift, 0.0, 0.0);
+    vec3 PositionB = position + vec3(0.0, 0.0, -shift);
+
+    // Elevation
+    float elevation = getElevation(csm_Position.xz);
+    csm_Position.y = elevation;
+    PositionA.y = getElevation(PositionA.xz);
+    PositionB.y = getElevation(PositionB.xz);
+
+    // Normal
+    vec3 toA = normalize(PositionA - csm_Position);
+    vec3 toB = normalize(PositionB - csm_Position);
+    csm_Normal = cross(toA, toB);
+
+    vPosition = csm_Position;
+    vPosition.xz += uTime * timeSpeed;
+    vUpDot = dot(csm_Normal, vec3(0.0, 1.0, 0.0));
 }
--- src/routes/lessons/44/+page.svelte
+++ src/routes/lessons/44/+page.svelte
@@ -9,8 +9,8 @@
   import { base } from "$app/paths";
   import { Brush, Evaluator, SUBTRACTION } from "three-bvh-csg";
   import CustomShaderMaterial from "three-custom-shader-material/vanilla";
-  // import terrainVertexShader from "$lib/shaders/procedural/vertex.glsl";
-  // import terrainFragmentShader from "$lib/shaders/procedural/fragment.glsl";
+  import terrainVertexShader from "$lib/shaders/procedural/vertex.glsl";
+  import terrainFragmentShader from "$lib/shaders/procedural/fragment.glsl";
 
   let canvas: any = null;
 
@@ -75,27 +75,141 @@
     /**
      * Terrain
      */
+    // Colors
+    const colors = {
+      colorWaterDeep: "#002b3d",
+      colorWaterSurface: "#66a8ff",
+      colorSand: "#ffe894",
+      colorGrass: "#85d534",
+      colorSnow: "#ffffff",
+      colorRock: "#bfbd8d",
+    };
     // Geometry
     const geometry = new THREE.PlaneGeometry(10, 10, 500, 500);
+    geometry.deleteAttribute("uv");
+    geometry.deleteAttribute("normal");
     geometry.rotateX(-Math.PI / 2);
+    // Uniforms
+    const uniforms = {
+      uTime: new THREE.Uniform(0),
+      // Position
+      uPositionFrequency: new THREE.Uniform(0.2),
+      uStrength: new THREE.Uniform(2.0),
+      // Warp
+      uWarpFrequency: new THREE.Uniform(5.0),
+      uWarpStrength: new THREE.Uniform(0.5),
+      // Colors
+      uColorWaterDeep: new THREE.Uniform(
+        new THREE.Color(colors.colorWaterDeep)
+      ),
+      uColorWaterSurface: new THREE.Uniform(
+        new THREE.Color(colors.colorWaterSurface)
+      ),
+      uColorSand: new THREE.Uniform(new THREE.Color(colors.colorSand)),
+      uColorGrass: new THREE.Uniform(new THREE.Color(colors.colorGrass)),
+      uColorSnow: new THREE.Uniform(new THREE.Color(colors.colorSnow)),
+      uColorRock: new THREE.Uniform(new THREE.Color(colors.colorRock)),
+    };
+
+        /**
+     * Water
+     */
+     const water = new THREE.Mesh(
+      new THREE.PlaneGeometry(10, 10, 1, 1),
+      new THREE.MeshPhysicalMaterial({
+        color: colors.colorWaterSurface,
+        metalness: 0.1,
+        roughness: 0.3,
+        transmission: 0.95,
+      })
+    );
+    water.rotation.x = -Math.PI / 2;
+    water.position.y = -0.1;
+    water.receiveShadow = true;
+    scene.add(water);
+
+    // Position
+    const guiPosition = gui.addFolder("Position");
+    guiPosition
+      .add(uniforms.uPositionFrequency, "value", 0, 1, 0.01)
+      .name("Frequency");
+    guiPosition.add(uniforms.uStrength, "value", 0, 5, 0.01).name("Strength");
+    // Warp
+    const guiWarp = gui.addFolder("Warp");
+    guiWarp
+      .add(uniforms.uWarpFrequency, "value", 0, 10, 0.01)
+      .name("Frequency");
+    guiWarp.add(uniforms.uWarpStrength, "value", 0, 1, 0.01).name("Strength");
+    // Colors
+    const guiColors = gui.addFolder("Colors");
+    guiColors
+      .addColor(colors, "colorWaterDeep")
+      .name("Water Deep")
+      .onChange((value: any) => {
+        uniforms.uColorWaterDeep.value.set(value);
+      });
+    guiColors
+      .addColor(colors, "colorWaterSurface")
+      .name("Water Surface")
+      .onChange((value: any) => {
+        uniforms.uColorWaterSurface.value.set(value);
+        water.material.color.set(value);
+      });
+    guiColors
+      .addColor(colors, "colorSand")
+      .name("Sand")
+      .onChange((value: any) => {
+        uniforms.uColorSand.value.set(value);
+      });
+    guiColors
+      .addColor(colors, "colorGrass")
+      .name("Grass")
+      .onChange((value: any) => {
+        uniforms.uColorGrass.value.set(value);
+      });
+    guiColors
+      .addColor(colors, "colorSnow")
+      .name("Snow")
+      .onChange((value: any) => {
+        uniforms.uColorSnow.value.set(value);
+      });
+    guiColors
+      .addColor(colors, "colorRock")
+      .name("Rock")
+      .onChange((value: any) => {
+        uniforms.uColorRock.value.set(value);
+      });
+
     // Material
     const material = new CustomShaderMaterial({
       // CSM
       baseMaterial: THREE.MeshStandardMaterial,
-      // vertexShader: terrainVertexShader,
-      // fragmentShader: terrainFragmentShader,
+      vertexShader: terrainVertexShader,
+      fragmentShader: terrainFragmentShader,
       silent: true,
+      uniforms,
+
       // MeshStandardMaterial
       metalness: 0,
       roughness: 0.5,
       color: "#85d534",
-    })
+    });
+    const depthMaterial = new CustomShaderMaterial({
+      // CSM
+      baseMaterial: THREE.MeshDepthMaterial,
+      vertexShader: terrainVertexShader,
+      silent: true,
+      uniforms,
+      // MeshDepthMaterial
+      depthPacking: THREE.RGBADepthPacking,
+    });
+
     // Mesh
     const terrain = new THREE.Mesh(geometry, material);
+    terrain.customDepthMaterial = depthMaterial;
     terrain.receiveShadow = true;
     terrain.castShadow = true;
     scene.add(terrain);
-
 
     /**
      * Lights
@@ -139,6 +253,7 @@
     // Animation
     const tick = () => {
       const elapsedTime = clock.getElapsedTime();
+      uniforms.uTime.value = elapsedTime;
       controls.update(); // NEW
       renderer.render(scene, camera);
       window.requestAnimationFrame(tick);
@@ -150,6 +265,10 @@
   onMount(() => {
     if (browser) {
       start();
+
+      return () => {
+        gui?.destroy();
+      };
     }
   });
 </script>
